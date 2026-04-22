604f7930438e1e8e189172f8da6ad79441d06cb9
lahirua-madhusanka <lahirua.madhusanka@gmail.com>
2026-04-22 11:16:53 +0530

    fixed vite.js

--- frontend-user/vite.config.js
+++ frontend-user/vite.config.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: __dirname,
-  base: "/",,
+  base: process.env.VITE_BASE_PATH || "/",
   plugins: [react()],
   resolve: {
     dedupe: ["react", "react-dom"],
